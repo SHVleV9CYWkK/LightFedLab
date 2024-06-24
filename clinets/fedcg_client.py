7d234023c0f9a02b49f9e285824cf9304b089579
SHVleV9CYWkK <835862228@qq.com>
2024-06-24 10:43:06 +1000

    Add error compensation

--- clinets/fedcg_client.py
+++ clinets/fedcg_client.py
@@ -6,6 +6,7 @@ class FedCGClient(Client):
     def __init__(self, client_id, dataset_index, full_dataset, bz, lr, epochs, criterion, device, **kwargs):
         super().__init__(client_id, dataset_index, full_dataset, bz, lr, epochs, criterion, device)
         self.compression_ratio = kwargs.get('compression_ratio', 0.5)
+        self.gradient_residuals = None
 
     def compress_gradients(self, param_changes):
         # 计算权重参数的总数量和top-k阈值
@@ -24,6 +25,11 @@ class FedCGClient(Client):
                 layer_top_k = max(int(num_elements / total_params * global_top_k), 1)
                 abs_grad = change.abs().flatten()
                 top_values, top_indices = torch.topk(abs_grad, layer_top_k, largest=True, sorted=False)
+
+                mask = torch.zeros_like(change.flatten(), dtype=torch.bool)
+                mask[top_indices] = True
+                mask = mask.reshape(change.shape)
+                self.gradient_residuals[name] = change * (~mask)
 
                 if next(self.model.parameters()).is_cuda or next(self.model.parameters()).is_cpu:
                     relevant_indices = (top_indices >= idx_offset) & (top_indices < idx_offset + num_elements)
@@ -45,6 +51,8 @@ class FedCGClient(Client):
         self.model.train()
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
         initial_params = {name: param.clone() for name, param in self.model.named_parameters()}
+        if self.gradient_residuals is None:
+            self.gradient_residuals = {name: torch.zeros_like(param) for name, param in self.model.named_parameters()}
         for epoch in range(self.epochs):
             for x, labels in self.client_train_loader:
                 x, labels = x.to(self.device), labels.to(self.device)
@@ -52,6 +60,9 @@ class FedCGClient(Client):
                 outputs = self.model(x)
                 loss = self.criterion(outputs, labels)
                 loss.backward()
+                for name, param in self.model.named_parameters():
+                    if param.grad is not None:
+                        param.grad += self.gradient_residuals[name]
                 optimizer.step()
 
         param_changes = {name: initial_params[name] - param.data for name, param in self.model.named_parameters()}
