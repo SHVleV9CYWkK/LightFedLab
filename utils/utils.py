462c18f980aa60b773d58fe244c720342a97a5c5
SHVleV9CYWkK <835862228@qq.com>
2024-07-18 17:01:00 +1000

    fixed bugs of Yahoo Answers dataset class

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,5 @@
 import os
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from torchvision.datasets import CIFAR10, CIFAR100, EMNIST, MNIST
 from utils.yahoo import YahooAnswersDataset
 from torchvision import transforms
@@ -93,37 +91,6 @@ def get_client_data_indices(root_dir, dataset_name, split_method):
 
     return client_indices, num_clients
 
-
-def plot_training_results(base_path, result_path=None, metrics=None):
-    if metrics is None:
-        metrics = ['accuracy', 'f1', 'loss', 'precision', 'recall']
-
-    if result_path is None:
-        result_path = base_path.replace('logs', 'result_image')
-    os.makedirs(result_path, exist_ok=True)  # 创建结果图片目录
-
-    # 读取所有的方法目录
-    methods = [dir for dir in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, dir))]
-
-    for metric in metrics:
-        plt.figure(figsize=(10, 6))
-
-        # 遍历每个方法，并读取相应的度量文件
-        for method in methods:
-            metric_path = os.path.join(base_path, method, f'{metric}.txt')
-            if os.path.exists(metric_path):
-                data = pd.read_csv(metric_path, header=None)
-                plt.plot(data, label=method)
-
-        plt.title(f'Training {metric.capitalize()} Over Epochs')
-        plt.xlabel('Epoch')
-        plt.ylabel(metric.capitalize())
-        plt.legend()
-        plt.grid(True)
-
-        # 保存图像
-        plt.savefig(os.path.join(result_path, f'{metric}.png'))
-        plt.close()
 
 
 def get_optimizer(optimizer_name, model, lr):
--- visualize_results.py
+++ visualize_results.py
@@ -1,6 +1,42 @@
-from utils.utils import plot_training_results
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
 from utils.args import parse_args_for_visualization
+
+
+def plot_training_results(base_path, result_path=None, metrics=None):
+    if metrics is None:
+        metrics = ['accuracy', 'f1', 'loss', 'precision', 'recall']
+
+    if result_path is None:
+        result_path = base_path.replace('logs', 'result_image')
+    os.makedirs(result_path, exist_ok=True)  # 创建结果图片目录
+
+    # 读取所有的方法目录
+    methods = [dir for dir in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, dir))]
+
+    for metric in metrics:
+        plt.figure(figsize=(10, 6))
+
+        # 遍历每个方法，并读取相应的度量文件
+        for method in methods:
+            metric_path = os.path.join(base_path, method, f'{metric}.txt')
+            if os.path.exists(metric_path):
+                data = pd.read_csv(metric_path, header=None)
+                plt.plot(data, label=method)
+
+        plt.title(f'Training {metric.capitalize()} Over Epochs')
+        plt.xlabel('Epoch')
+        plt.ylabel(metric.capitalize())
+        plt.legend()
+        plt.grid(True)
+
+        # 保存图像
+        plt.savefig(os.path.join(result_path, f'{metric}.png'))
+        plt.close()
+
 
 if __name__ == '__main__':
     arguments = parse_args_for_visualization()
     plot_training_results(arguments.log_dir, arguments.save_dir, ['accuracy', 'loss'])
+
