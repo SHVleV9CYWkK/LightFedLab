f574c69625287fcef1bf5f27e665a12a7bef2fa2
SHVleV9CYWkK <835862228@qq.com>
2024-08-23 11:11:49 +1000

    Modify the grid search

--- grid_search.py
+++ grid_search.py
@@ -34,18 +34,48 @@ def run_and_capture(params):
 if __name__ == '__main__':
     # 参数空间
     param_grid = {
-        "fl_method": ["fedmask"],
+        "fl_method": ["adfedmask"],
         "dataset_name": ["emnist"],
         "model": ["leafcnn1"],
         "local_epochs": [1],
-        "lr": [0.05, 0.01, 0.005, 0.001, 0.0005, 0.001],
+        "lr": [0.005, 0.001, 0.0005, 0.0001],
         "batch_size": [128],
         "n_rounds": [20],
         "seed": [42],
         "device": ["cuda"],
         "split_method": ["clusters"],
-        "optimizer_name": ["sgd", "adam"],
+        "optimizer_name": ["adam"],
         "n_job": [12]
+    }
+
+    # 生成所有参数组合
+    keys, values = zip(*param_grid.items())
+    param_combinations = [dict(zip(keys, v)) for v in product(*values)]
+
+    # 记录每个参数组合的最大准确率
+    results = []
+
+    # 遍历所有参数组合，执行并记录最大准确率
+    for params in param_combinations:
+        max_accuracy = run_and_capture(params)
+        results.append((params, max_accuracy))
+        with open("grid_search_results.txt", "a") as file:
+            file.write(f"Params: {params}, Max Accuracy: {max_accuracy}\n")
+
+    # 参数空间
+    param_grid = {
+        "fl_method": ["adfedmask"],
+        "dataset_name": ["cifa10"],
+        "model": ["lenet"],
+        "local_epochs": [1],
+        "lr": [0.005, 0.001, 0.0005, 0.0001],
+        "batch_size": [128],
+        "n_rounds": [100],
+        "seed": [42],
+        "device": ["cuda"],
+        "split_method": ["clusters"],
+        "optimizer_name": ["adam"],
+        "n_job": [0]
     }
 
     # 生成所有参数组合
